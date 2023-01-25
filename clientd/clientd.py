2134a404bb8e55fb819238e9f87ce52a14568c7f
jlourenco <jlourenco@PTS-LT-H70.schreder.loc>
2023-01-25 23:41:46 +0000

    added

--- clientd/clientd.py
+++ clientd/clientd.py
@@ -1,11 +1,20 @@
 import os
 import zmq
+import logging
+
+logging.basicConfig(filename="output.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info("Starting ZeroMQ Client ...")
 
 context = zmq.Context()
 socket = context.socket(zmq.PULL)
 address = os.environ.get('SERVER_CONNECT_URI')
 socket.connect(address)
-print("Listening to {}...".format(address))
+logging.info("Listening to {}...".format(address))
 while True:
     message = socket.recv_string()
-    print("Client got message! {}".format(message))
+    logging.info("Client got message! {}".format(message))
--- serverd/serverd.py
+++ serverd/serverd.py
@@ -1,13 +1,25 @@
 import os
 import zmq
 import time
+import logging
+
+logging.basicConfig(filename="output.log",
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
+logging.info("Starting ZeroMQ Server ...")
 
 context = zmq.Context()
 socket = context.socket(zmq.PUSH)
 address = os.environ.get('SERVER_LISTEN_URI')
 socket.bind(address)
-print("Sending to {}...".format(address))
+logging.info("Sending to {}...".format(address))
+counter = 0
 while True:
-    message = socket.send_string("Got it!")
-    print("Sent message")
+    msg = "Got it: {0}".format(counter)
+    counter = counter + 1
+    message = socket.send_string(msg)
+    logging.info("Sent message: '{0}'".format(msg))
     time.sleep(1)
